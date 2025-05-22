277fc80c4c561d76f607207cc897c72c4c74f88c
hojunlee123 <86946813+hojunlee123@users.noreply.github.com>
2025-05-22 15:36:31 +0900

    Title: 함수를 이용한 369 게임, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 250522/함수를 이용한 369 게임/369-games-using-functions.c
+++ 250522/함수를 이용한 369 게임/369-games-using-functions.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+bool contains369(int n) {
+    while (n > 0) {
+        int digit = n % 10;
+        if (digit == 3 || digit == 6 || digit == 9)
+            return true;
+        n /= 10;
+    }
+    return false;
+}
 
 int main() {
     int a, b;
     scanf("%d %d", &a, &b);
-    int k=0;
-    for(int j=a;j<=b;j++){
-        if(j%10==3 ||j%10==6 ||j%10==9 || j/10==3){
+
+    int k = 0;
+    for (int j = a; j <= b; j++) {
+        if (contains369(j) || j % 3 == 0) {
             k++;
         }
-        else{
-            if(j%3==0){
-                k++;
-            }
-        }
-
-        
     }
-    
-    printf("%d",k);
 
+    printf("%d\n", k);
     return 0;
 }
