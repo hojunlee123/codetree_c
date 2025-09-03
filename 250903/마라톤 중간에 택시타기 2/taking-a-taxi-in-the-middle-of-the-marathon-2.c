f4aba9f1430414581e60285bc0761add53b1d311
hojunlee123 <86946813+hojunlee123@users.noreply.github.com>
2025-09-03 23:59:52 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250903/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+char binary[11];
+
+int toDecimal(char *str) {
+    int value = 0;
+    for (int i = 0; str[i]; i++) {
+        value = value * 2 + (str[i] - '0');
+    }
+    return value;
+}
+
+int main() {
+    scanf("%s", binary);
+    int len = strlen(binary);
+    int max_value = 0;
+
+    for (int i = 0; i < len; i++) {
+        char temp[11];
+        strcpy(temp, binary);
+
+        // Flip the i-th bit
+        temp[i] = (temp[i] == '0') ? '1' : '0';
+
+        int val = toDecimal(temp);
+        if (val > max_value) {
+            max_value = val;
+        }
+    }
+
+    printf("%d\n", max_value);
+    return 0;
+}
