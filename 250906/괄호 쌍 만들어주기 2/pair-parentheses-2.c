a9f96a25174725f5cc8ce21e100b5f75fdecfc55
hojunlee123 <86946813+hojunlee123@users.noreply.github.com>
2025-09-06 23:06:29 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250906/괄호 쌍 만들어주기 2/pair-parentheses-2.c
+++ 250906/괄호 쌍 만들어주기 2/pair-parentheses-2.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-char str[101];
+char str[105];
 
 int main() {
     scanf("%s", str);
     int n = strlen(str);
 
-    int open = 0;
+    int open_pairs = 0;
     int ans = 0;
 
-    for (int i = 0; i < n; i++) {
-        if (str[i] == '(') {
-            open++;
-        } else if (str[i] == ')') {
-            ans += open;
+    for (int i = 0; i < n - 1; i++) {
+        if (str[i] == '(' && str[i+1] == '(') {
+            open_pairs++;
+        }
+        if (str[i] == ')' && str[i+1] == ')') {
+            ans += open_pairs;
         }
     }
 
