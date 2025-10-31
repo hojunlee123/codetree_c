5aa08236b96397606ee12ab10911eb88d514d150
hojunlee123 <86946813+hojunlee123@users.noreply.github.com>
2025-10-31 20:18:04 +0900

    Title: 가장 작은 x 찾기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251031/가장 작은 x 찾기/find-smallest-x.c
+++ 251031/가장 작은 x 찾기/find-smallest-x.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
-#include <math.h>
 
-int n;
-int a[10], b[10];
+typedef struct {
+    int start, end;
+} Interval;
 
 int main() {
+    int n;
     scanf("%d", &n);
+    Interval arr[105];
     for (int i = 0; i < n; i++)
-        scanf("%d %d", &a[i], &b[i]);
+        scanf("%d %d", &arr[i].start, &arr[i].end);
 
-    double low = a[n - 1], high = b[n - 1];
-
-    for (int i = n - 2; i >= 0; i--) {
-        low = ceil(low / 2.0);
-        high = floor(high / 2.0);
-
-        if (low < a[i]) low = a[i];
-        if (high > b[i]) high = b[i];
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i].start > arr[j].start) {
+                Interval tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
     }
 
-    printf("%d", (int)low);
+    int cnt = 1;
+    int cur_start = arr[0].start;
+    int cur_end = arr[0].end;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i].start <= cur_end) {
+            if (arr[i].end > cur_end)
+                cur_end = arr[i].end;
+        } else {
+            cnt++;
+            cur_start = arr[i].start;
+            cur_end = arr[i].end;
+        }
+    }
+
+    printf("%d\n", cnt);
     return 0;
 }
